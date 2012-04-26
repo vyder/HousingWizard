951f9fc87f46088ade3dc403f3595fd9e0a9c55d
Vidur Murali <vidur.murali@gmail.com>
2012-04-26 17:01:26 -0400

    Rails app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HousingWizard::Application.config.secret_token = '79c85d29d083fe14d834383eb02a47c91c8829a06c43fd553bdb5df2f9114aabe3aae21c758013529cd49440164b8b44997968ff6ab2fd65e81d79996afa5b0b'
