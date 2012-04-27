1cae59c37dbf6f6a8137e7d0df2670dd7af258ac
Vidur Murali <vidur.murali@gmail.com>
2012-04-27 11:05:36 -0400

    I think i've got it now

--- config/application.rb
+++ config/application.rb
@@ -52,9 +52,8 @@ module HousingWizard
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    # config.assets.version = '1.0'
+    config.assets.version = '1.0'
   end
 end
