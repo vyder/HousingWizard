7ce971cac9fb1e8126a096ddde0c0aa9dbfcf5e9
Vidur Murali <vidur.murali@gmail.com>
2012-04-26 23:01:30 -0400

    Compiled again

--- Gemfile
+++ Gemfile
@@ -6,12 +6,12 @@ gem 'rails', '3.2.1'
 #gem 'sqlite3'
 #gem 'taps', '~> 0.3.23'
 gem 'therubyracer', '~> 0.10.1'
-gem 'twitter-bootstrap-rails', '~> 2.0.6'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem 'sass-rails',   '~> 3.2.3'
+  gem 'twitter-bootstrap-rails', '~> 2.0.6'
   gem 'uglifier', '>= 1.0.3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,8 @@ HousingWizard::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( jquery.js jquery-ui.js jquery_ujs.js application.css )
+  config.assets.precompile << '*.js'
+  config.assets.precompile << '*.css'
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
